e09a2c40355f7b30e48a5eab040b8a8d2157e081
rzhasibuan <rzhasibuan@gmail.com>
2022-10-10 22:55:26 +0700

    membuat module sayHello update sayhello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rzhasibuan/go-say-hello
+module github.com/rzhasibuan/go-say-hello/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
 func SayHello(name string) string {
-	return "Hello bro" + name
+	return "Hello bro " + name + " selamat datang"
 }
